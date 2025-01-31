b38db6e30a432a8cdeae7faf08903e148f5f0050
Kayro Barbosa <kayromatheus584@gmail.com>
2025-01-31 12:24:38 +0000

    Ajustes visuais na página portfolio

--- src/components/portfolio/PortfolioBlock.js
+++ src/components/portfolio/PortfolioBlock.js
@@ -22,47 +22,55 @@ function PortfolioBlock({ image, source, title, description }) {
                 src={image} 
                 alt={title} 
                 width="100%" 
-                sx={{ borderRadius: '10px' }}
+                sx={{ 
+                    borderRadius: '10px',
+                    display: 'block',
+                    transition: 'opacity 0.3s ease',
+                    opacity: hover ? 0.3 : 1 // Reduz a opacidade ao passar o cursor
+                }}
             />
 
             {/* Sobreposição ao passar o cursor */}
-            {hover && (
-                <Box 
-                    position="absolute" 
-                    top={0} left={0} width="100%" height="100%"
-                    bgcolor="rgba(0, 0, 0, 0.8)"
-                    display="flex" flexDirection="column"
-                    justifyContent="center" alignItems="center"
-                    sx={{ borderRadius: '10px' }}
+            <Box 
+                position="absolute" 
+                top={0} left={0} width="100%" height="100%"
+                display="flex" flexDirection="column"
+                justifyContent="center" alignItems="center"
+                sx={{ 
+                    backgroundColor: "rgba(0, 0, 0, 0.8)", 
+                    borderRadius: '10px', 
+                    opacity: hover ? 1 : 0, 
+                    transition: 'opacity 0.3s ease',
+                    zIndex: 2
+                }}
+            >
+                <Typography 
+                    variant="h6" 
+                    color="#00ffa4"
+                    fontWeight="bold"
+                    sx={{ mb: 2 }} 
                 >
-                    <Typography 
-                        variant="h6" 
-                        color="#00ffa4"
-                        fontWeight="bold"
-                        sx={{ mb: 2 }} // Margem inferior para espaçamento
-                    >
-                        {title}
-                    </Typography>
-                    <Typography color="#ffffff" textAlign="center" sx={{ px: 2, mb: 2 }}>
-                        {description}
-                    </Typography>
-                    <Button 
-                        variant="contained" 
-                        href={source} 
-                        target="_blank" 
-                        sx={{
-                            bgcolor: '#6b47f0',
-                            '&:hover': { bgcolor: 'rgba(108, 71, 240, 0.7)' },
-                            display: 'flex',
-                            alignItems: 'center',
-                            gap: '0.5rem'
-                        }}
-                     >
-                        <i className='fa fa-code' style={{ fontSize: '1rem' }} />
-                           Source Code
-                     </Button>
-                </Box>
-            )}
+                    {title}
+                </Typography>
+                <Typography color="#ffffff" textAlign="center" sx={{ px: 2, mb: 2 }}>
+                    {description}
+                </Typography>
+                <Button 
+                    variant="contained" 
+                    href={source} 
+                    target="_blank" 
+                    sx={{
+                        bgcolor: '#6b47f0',
+                        '&:hover': { bgcolor: 'rgba(108, 71, 240, 0.7)' },
+                        display: 'flex',
+                        alignItems: 'center',
+                        gap: '0.5rem'
+                    }}
+                >
+                    <i className='fa fa-code' style={{ fontSize: '1rem' }} />
+                    Source Code
+                </Button>
+            </Box>
         </Box>
     );
 }
--- src/info/Info.js
+++ src/info/Info.js
@@ -5,7 +5,7 @@ import mock3 from "../img/mock3.png"
 import mock4 from "../img/mock4.png"
 import mock5 from "../img/mock5.png"
 
-export let colors = [" #00ffa4", " #a668ff"]; // paleta de cores - #00ffa4 #6b47f0
+export let colors = [" #00ffa4", " #a668ff"]; 
 
 export let singlePage = false;
 
@@ -52,33 +52,18 @@ export const info = {
             icon: "fa fa-twitter",
             label: 'twitter'
         }
-
     ],
     bio: "Hello! I'm Kayro. I'm a future software developer. I'm studying Web Development and Multimedia at IPCA, currently searching for an internship.",
-    skills:
-        {
-            proficientWith: ['c#', 'javascript', 'flutter', 'git', 'github', 'html5', 'css3', 'unity'],
-            exposedTo: ['nodejs', 'typescript', 'docker', 'angular'],
-            softSkills: ['teamwork', 'time-management', 'adaptability', 'resilience', 'problem-solving', 'proactivity']
-        }
-    ,
+    skills: {
+        proficientWith: ['c#', 'javascript', 'flutter', 'git', 'github', 'html5', 'css3', 'unity'],
+        exposedTo: ['nodejs', 'typescript', 'docker', 'angular'],
+        softSkills: ['teamwork', 'time-management', 'adaptability', 'resilience', 'problem-solving', 'proactivity']
+    },
     hobbies: [
-        {
-            label: 'gaming',
-            emoji: '🎮'
-        },
-        {
-            label: 'football',
-            emoji: '⚽'
-        },
-        {
-            label: 'cooking',
-            emoji: '🔪'
-        },
-        {
-            label: 'music',
-            emoji: '🎵'
-        }
+        { label: 'gaming', emoji: '🎮' },
+        { label: 'football', emoji: '⚽' },
+        { label: 'cooking', emoji: '🔪' },
+        { label: 'music', emoji: '🎵' }
     ],
     portfolio: [
         {
@@ -86,35 +71,40 @@ export const info = {
             live: "https://kayrobarbosa.netlify.app",
             source: "https://github.com/kallismb/kayro-portfolio",
             image: mock1,
-            description: "This is my personal portfolio, showcasing the some of the projects I have worked on. (University Project - Web Design Introduction)"
+            description: "This is my personal portfolio, showcasing some of the projects I have worked on.",
+            technologies: ['JavaScript', 'React', 'SCSS']
         },
         {
             title: "Gastronomia & Clima",
             live: "#",
             source: "https://github.com/kallismb/gastronomia-clima",
             image: mock2,
-            description: "A web app using NodeJS and Supabase database that connects gastronomy and weather APIs to suggest restaurants based on the weather. (University Project - Distributed Computing)"
+            description: "A web app using NodeJS and Supabase database.",
+            technologies: ['JavaScript', 'NodeJS', 'ExpressJS', 'Supabase', 'Docker', 'CSS']
         },
         {
             title: "Stock App",
             live: "#",
             source: "https://github.com/kallismb/stock-app",
             image: mock3,
-            description: "A React app for tracking stock prices with real-time data integration. (University Project - Web Development II)"
+            description: "A React app for tracking stock prices.",
+            technologies: ['JavaScript', 'React', 'CSS']
         },
         {
             title: "Gestão Livraria",
             live: "#",
             source: "https://github.com/kallismb/gestao-livraria",
             image: mock4,
-            description: "A C# .NET Console App for managing a library system. (University Project - Programming Languages I)"
+            description: "A C# .NET Console App for managing a library system.",
+            technologies: ['C#']
         },
         {
             title: "XP Academy",
             live: "#",
             source: "https://www.behance.net/gallery/218138889/XP-Academy-Projeto",
             image: mock5,
-            description: "An e-learning gamified platform to develop skills and growth in various subjects. (University Project - Web Design Introduction)"
+            description: "An e-learning gamified platform to develop skills.",
+            technologies: ['Figma']
         }
     ]
 }
